1cd5d128ba247ee96a301e28162b8e48b9330ebb
KangHyun Lee <rkd7327@naver.com>
2018-03-23 09:43:05 +0900

    연결테스트 완료

---
+++ src/test/java/com/dgit/poto/DataSourceTest.java
@@ -0,0 +1,35 @@
+package com.dgit.poto;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/*.xml" })
+public class DataSourceTest {
+
+	@Autowired
+	private DataSource ds;
+
+	@Test
+	public void testConnection() {
+		Connection conn = null;
+
+		try {
+			conn = ds.getConnection();
+			System.out.println(conn);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
